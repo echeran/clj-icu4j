521c8e4228afba2eb7cbb562861cd3933eb3ae3a
Elango Cheran <elango@google.com>
2018-07-26 15:28:14 -0700

    more testing of plural rules for extent of data availability

--- src/clj_icu4j/plural.clj
+++ src/clj_icu4j/plural.clj
@@ -1,5 +1,6 @@
 (ns clj-icu4j.plural
-  (:require [clj-icu4j.locale :as locale])
+  (:require [clj-icu4j.locale :as locale]
+            [clojure.set :as set])
   (:import [com.ibm.icu.text PluralRules PluralRules$PluralType]))
 
 (def PLURAL-TYPES {::cardinal PluralRules$PluralType/CARDINAL
@@ -8,3 +9,10 @@
 (defn plural-rules-for-plural-type
   [locale plural-type]
   (PluralRules/forLocale locale plural-type))
+
+(defn plural-type-display-name
+  [plural-type]
+  (let [plural-type-to-keywords (set/map-invert PLURAL-TYPES)
+        plural-type-keyword (get plural-type-to-keywords plural-type)
+        plural-type-name (name plural-type-keyword)]
+    plural-type-name))
--- src/clj_icu4j/scratch.clj
+++ src/clj_icu4j/scratch.clj
@@ -5,12 +5,21 @@
 ;; This ns is for playing around.  Aka "playground" in Java
 ;; projects.
 
-(defn print-plural-keywords
-  "Given a Locale and PluralType, get the plural rules, and print out the locale, PluralType, and all of the rule's keywords"
+(defn get-plural-keywords
+  "Given a Locale and PluralType, get the plural rules"
   [locale plural-type]
   (let [plural-rules (plural/plural-rules-for-plural-type locale plural-type)
         keywords (.getKeywords plural-rules)]
-    (println "Locale:" locale ", plural type:" plural-type)
+    keywords))
+
+(defn print-plural-keywords
+  "Given a Locale and PluralType, get the plural rules, and print out the locale, PluralType, and all of the rule's keywords"
+  [locale plural-type]
+  (let [keywords (get-plural-keywords locale plural-type)]
+    (println (format "Locale: %s (%s), plural type: %s"
+                     (.getDisplayName locale)
+                     (.toLanguageTag locale)
+                     (plural/plural-type-display-name plural-type)))
     (doseq [k keywords]
       (println "\t" k))))
 
