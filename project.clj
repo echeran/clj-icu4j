80476596c917257357ba6cf40d57177442232881
Elango Cheran <elango@google.com>
2018-07-26 14:50:54 -0700

    upgrades ICU version

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [com.ibm.icu/icu4j "60.1"]])
+                 [com.ibm.icu/icu4j "62.1"]])
