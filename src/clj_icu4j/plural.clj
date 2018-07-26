60fdb05b3d37022783cde4914c156e1edf43909f
Elango Cheran <elango@google.com>
2018-07-26 14:52:41 -0700

    fills in some code for plural ns

--- src/clj_icu4j/plural.clj
+++ src/clj_icu4j/plural.clj
@@ -1,1 +1,10 @@
-(ns clj-icu4j.plural)
+(ns clj-icu4j.plural
+  (:require [clj-icu4j.locale :as locale])
+  (:import [com.ibm.icu.text PluralRules PluralRules$PluralType]))
+
+(def PLURAL-TYPES {::cardinal PluralRules$PluralType/CARDINAL
+                   ::ordinal PluralRules$PluralType/ORDINAL})
+
+(defn plural-rules-for-plural-type
+  [locale plural-type]
+  (PluralRules/forLocale locale plural-type))
