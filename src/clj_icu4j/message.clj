f7b687d995e07c316305a4d98dfb70a6386e99f0
Elango Cheran <elango@google.com>
2019-06-08 16:07:07 -0700

    adds wrapper for MessageFormat

---
+++ src/clj_icu4j/message.clj
@@ -0,0 +1,85 @@
+(ns clj-icu4j.message
+  (:refer-clojure :exclude [format])
+  (:require [clojure.string :as string])
+  (:import com.ibm.icu.text.MessageFormat))
+
+(defn selection-pattern-1-arg
+  "Return the String pattern for MessageFormat when a selection options map is provided"
+  [selection-map]
+  (let [{:keys [select-args select-cases]} selection-map]
+    (assert (= 1 (count select-args)) "More args than I can currently handle (but I can make myself recursive to handle N=2+ args, of course)")
+    (let [first-arg (first select-args)
+          cases-str (->> (for [[k v] select-cases]
+                           (let [scalar-case-val (if (sequential? k) (first k) k)]
+                             (str scalar-case-val " {" v "}")))
+                         (string/join " "))
+          inner-str (if (<= (count first-arg) 2)
+                      (let [arg-name (name (first first-arg))
+                            arg-type (name (second first-arg))
+                            pattern-parts [arg-name arg-type cases-str]
+                            comma-sep-parts-str (string/join ", " pattern-parts)]
+                        comma-sep-parts-str)
+                      (let [comma-sep-parts-str (string/join ", " (map name first-arg))
+                            pattern-parts [comma-sep-parts-str
+                                           cases-str]]
+                        (string/join "\n" pattern-parts)))
+          pattern-str (str "{" inner-str "}")]
+      pattern-str)))
+
+(defn selection-pattern-multi-arg
+  "Same as selection-pattern-1-arg, but allow for multiple args.
+  The selection cases map should be keyed by vectors of case values in the same order of the selection args that they represent."
+  [selection-map]
+  (let [select-args (:select-args selection-map)
+        select-cases (:select-cases selection-map)]
+    (if (= 1 (count select-args))
+      (selection-pattern-1-arg selection-map)
+      (let [first-arg (first select-args)
+            cases-by-first-arg-val (->> (keys select-cases)
+                                        (group-by first))
+            case-sub-patterns (for [[first-arg-val matching-cases] cases-by-first-arg-val]
+                                (let [new-select-args (rest select-args)
+                                      new-select-cases (into {} (for [case matching-cases]
+                                                                  (let [new-case (rest case)
+                                                                        pattern (get select-cases case)]
+                                                                    [new-case pattern])))
+                                      new-selection-map (assoc selection-map
+                                                               :select-args new-select-args
+                                                               :select-cases new-select-cases)
+                                      ;; recursive call is here!
+                                      sub-pattern (selection-pattern-multi-arg new-selection-map)]
+                                  (str  first-arg-val " {" sub-pattern "}")))
+            case-sub-patterns-str (string/join "\n" case-sub-patterns)
+            first-arg-parts (concat (map name first-arg)
+                                    [case-sub-patterns-str])
+            first-arg-pattern (str "{" (string/join ", " first-arg-parts) "}")]
+        first-arg-pattern))))
+
+(defn ^MessageFormat formatter
+  "Instantiate a MessageFormat object by passing a String pattern or a selection options map."
+  [pattern-or-selection]
+  (if (string? pattern-or-selection)
+    (let [pattern pattern-or-selection]
+      (MessageFormat. pattern))
+    (do
+      (assert (map? pattern-or-selection) "You must pass either a String pattern or a selection map")
+      (let [selection-map pattern-or-selection
+            {:keys [locale select-args select-cases]} selection-map
+            pattern (selection-pattern-multi-arg selection-map)]
+        (MessageFormat. pattern locale)))))
+
+(defn ^String format
+  "Provide a localized interpolated string based on the MessageFormat object and the specific input values provided.
+Provide a sequential argument if the formatter pattern used numerical arguments. Else, provide a map for a selection pattern / named arugments."
+  [fmtr args]
+  (if (sequential? args)
+    (.format fmtr (to-array args))
+    (do
+      (assert (map? args) "You must pass either a sequential collection of values or a map of arg names and values.")
+      (let [{:keys []} args
+            named-args-map args
+            string-key-args-map (into {}
+                                      (for [[k v] named-args-map]
+                                        (let [arg-name (-> (name k))]
+                                          [arg-name v])))]
+        (.format fmtr string-key-args-map)))))
---
+++ test/clj_icu4j/message_test.clj
@@ -0,0 +1,90 @@
+(ns clj-icu4j.message-test
+  (:refer-clojure :exclude [format])
+  (:require [clojure.test :refer :all]
+            [clj-icu4j.message :as msg])
+  (:import com.ibm.icu.util.ULocale))
+
+;; Examples adapted from preamble of Javadoc entry for MessageFormat:
+;; http://icu-project.org/apiref/icu4j/com/ibm/icu/text/MessageFormat.html
+
+(deftest string-pattern-sequential-vals
+  (let [fmtr (msg/formatter "The disk \"{1}\" contains {0} file(s).")]
+    (is (= "The disk \"MyDisk\" contains 3 file(s)."
+           (msg/format fmtr [3 "MyDisk"])))
+    (is (= "The disk \"MyDisk\" contains 1 file(s)."
+           (msg/format fmtr [1 "MyDisk"])))
+    (is (= "The disk \"MyDisk\" contains 1,273 file(s)."
+           (msg/format fmtr [1273 "MyDisk"])))))
+  
+(deftest string-pattern-map-vals
+  (let [fmtr (msg/formatter "The disk \"{disk_name}\" contains {num_files} file(s).")]
+    (is (= "The disk \"MyDisk\" contains 3 file(s)."
+           (msg/format fmtr {"disk_name" "MyDisk"
+                             "num_files" 3})))
+    (is (= "The disk \"MyDisk\" contains 1 file(s)."
+           (msg/format fmtr {"disk_name" "MyDisk"
+                             "num_files" 1})))
+    (is (= "The disk \"MyDisk\" contains 1,273 file(s)."
+           (msg/format fmtr {"disk_name" "MyDisk"
+                             "num_files" 1273})))))
+
+(deftest selection-format-map-vals-1-arg
+  (let [fmtr (msg/formatter {:select-args [["num_files" :plural]]
+                             :select-cases {"=0"    "There are no files on disk \"{disk_name}\"."
+                                            "=1"    "There is one file on disk \"{disk_name}\"."
+                                            "other" "There are # files on disk \"{disk_name}\"."}
+                             :locale ULocale/ENGLISH})
+        args-map-1 {"num_files" 0
+                    "disk_name" "MyDisk"}]
+    (testing "use the args map from the Javadoc examples"
+      (is (= "There are no files on disk \"MyDisk\"."
+             (msg/format fmtr args-map-1))))
+    (testing "updating the args map based on Javadoc examples"
+      (let [args-map-2 (assoc args-map-1 "num_files" 3)]
+        (is (= "There are 3 files on disk \"MyDisk\"."
+           (msg/format fmtr args-map-2)))))))
+
+
+(deftest selection-format-map-vals-multi-arg
+  (testing "version 1 of simplifying multi-arg selection syntax"
+    (let [fmtr (msg/formatter {:select-args [["gender_of_host" "select"]
+                                             ["num_guests" "plural" "offset:1"]]
+                               :select-cases {["female" "=0"] "{host} does not give a party."
+                                              ["female" "=1"] "{host} invites {guest} to her party."
+                                              ["female" "=2"] "{host} invites {guest} and one other person to her party."
+                                              ["female" "other"] "{host} invites {guest} and # other people to her party."
+                                              ["male" "=0"] "{host} does not give a party."
+                                              ["male" "=1"] "{host} invites {guest} to his party."
+                                              ["male" "=2"] "{host} invites {guest} and one other person to his party."
+                                              ["male" "other"] "{host} invites {guest} and # other people to his party."
+                                              ["other" "=0"] "{host} does not give a party."
+                                              ["other" "=1"] "{host} invites {guest} to their party."
+                                              ["other" "=2"] "{host} invites {guest} and one other person to their party."
+                                              ["other" "other"] "{host} invites {guest} and # other people to their party."}
+                               :locale ULocale/ENGLISH})]
+      (let [args-map {"gender_of_host" "male"
+                      "host" "MC Hammer"
+                      "num_guests" 0
+                      ;;"guest" "DJ Jazzy Jeff"
+                      }]
+        (is (= "MC Hammer does not give a party."
+               (msg/format fmtr args-map))))
+      (let [args-map {"gender_of_host" "male"
+                      "host" "MC Hammer"
+                      "num_guests" 2
+                      "guest" "DJ Jazzy Jeff"
+                      }]
+        (is (= "MC Hammer invites DJ Jazzy Jeff and one other person to his party."
+               (msg/format fmtr args-map))))
+      (let [args-map {"gender_of_host" "female"
+                      "host" "Paula Poundstone"
+                      "num_guests" 1
+                      "guest" "Michelle Wolf"}]
+        (is (= "Paula Poundstone invites Michelle Wolf to her party."
+               (msg/format fmtr args-map))))
+      (let [args-map {"gender_of_host" "other"
+                      "host" "The Fry Guys"
+                      "num_guests" 8
+                      "guest" "Ronald McDonald"}]
+        (is (= "The Fry Guys invites Ronald McDonald and 7 other people to their party."
+               (msg/format fmtr args-map)))))))
