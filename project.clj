e66d4d00ec2b3181109a3b7c562b2b8d2fa02afc
Elango Cheran <elango@unicode.org>
2023-06-15 15:12:35 -0700

    add Leiningen option :repl-options to project.clj

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/math.numeric-tower "0.0.5"]
-                 [com.ibm.icu/icu4j "62.1"]])
+                 [com.ibm.icu/icu4j "62.1"]]
+  :repl-options {:init-ns clj-icu4j.calendar})
